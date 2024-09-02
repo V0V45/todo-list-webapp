5057ad8e2d060876b5ac007308bba6a22f36cf53
V0V45 <vovanfilatov@yandex.ru>
2024-09-02 13:41:39 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.module.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (<>
+    <header>
+      <img src='./images/logo.svg' alt='To-Do List' />
+    </header>
+    <main>
+
+    </main>
+  </>
   );
 }
 
